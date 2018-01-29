aa30d2c86afcb1443d85e6061530161f064915d3
Junior <juniorr1995@gmail.com>
2018-01-29 14:50:08 -0500

    Add basic example of an array of integers and how to display it

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = {1,3,56,4}; //Array with 4 elements, 3 indexes (0-3)
+
+            //Foreach loop to show all integers in array1
+            foreach (int swag in array1)
+            {
+                System.Console.WriteLine(swag);
+            }
+        }
+    }
+}
